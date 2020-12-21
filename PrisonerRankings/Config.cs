058414194076f758c575178c6dc7576e3f35b09f
Thundermaker300 <thundermaker300@gmail.com>
2020-12-21 16:18:00 -0500

    Adding descriptions to settings

--- PrisonerRankings/Config.cs
+++ PrisonerRankings/Config.cs
@@ -14,16 +14,21 @@ namespace PrisonerRankings
         public bool IsEnabled { get; set; } = true;
         [Description("Also enable the rating system for chaos.")]
         public bool EnableForChaos { get; set; } = false;
+        [Description("Determines whether or not stars will be displayed above a player's name representing their rating.")]
         public bool DisplayOnCharacter { get; set; } = true;
+        [Description("The message to show to all alive MTF when a player reaches a 3-star rating. Set to none to disable.")]
         public string ThreeRatingBroadcast { get; set; } = "{name} has reached a 3-star rating!";
+        [Description("The message to show to all alive MTF when a player reaches a 5-star rating. Set to none to disable.")]
         public string FiveRatingBroadcast { get; set; } = "{name} has reached a 5-star rating and must be terminated immediately!";
         [Description("Sets the message to announce when someone with a rating of 3 or higher is killed. Set to none to disable.")]
         public string TerminationBroadcast { get; set; } = "{name} has been terminated.";
+        [Description("Determines the amount of rating that is gained for each of the specified actions.")]
         public float PickupKeycard { get; set; } = 0.2f;
         public float PickupWeapon { get; set; } = 1f;
         public float PickupScpItem { get; set; } = 0.2f;
         public float EnterHcz { get; set; } = 1f;
         public float EnterEz { get; set; } = 1f;
+        [Description("Determines the amount of rating that is gained for killing users of certain classes. Add/remove classes to the list to include them (eg. SCPs).")]
         public Dictionary<RoleType, float> ClassRatings { get; set; } = new Dictionary<RoleType, float>
         {
             [RoleType.Scientist] = 0.5f,
